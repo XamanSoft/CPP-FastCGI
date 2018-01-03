d533b325fe0121f6cdfd56fd7482343226c43336
Mauro Joel Schütz <mjschutz@outlook.com>
2018-01-03 06:23:54 +0000

    Update Daemon Options

--- src/daemon_main.cpp
+++ src/daemon_main.cpp
@@ -3,6 +3,20 @@
 
 using namespace CppFastCGI;
 
+#ifdef _WIN32
+#define getcwd		_getdcwd
+#endif
+
 int main(int argc, char* argv[]) {
+	char cwd[1024];
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		CppSystemRT::Daemon::config("pidfile", std::string(cwd)+"/cppfcgi.pid");
+	}
+
+	if (argc > 1 && std::string(argv[1]) == "-q") {
+		CppSystemRT::Daemon::stop();
+		return 0;
+	}
+	
 	return CppSystemRT::Daemon::exec<DaemonThread>("127.0.0.1", "9000");
 }
